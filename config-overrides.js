0b6a00ba5f463dc84d1d963d32fbc112d9029942
agustin criado <ag-us1@hotmail.com>
2022-08-26 17:16:38 +0200

    a

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,10 @@
-module.exports = function () {
-  return {
-    webpack: {
-      configure: {
-        resolve: {
-          fallback: {
-            buffer: false
-          },
-        },
-      },
+module.exports = function override(config, env) {
+  
+  let loaders = config.resolve
+  loaders.splice(0, 0, {
+    fallback: {
+      buffer: false
     }
-  };
+  })
+  return config
 };
