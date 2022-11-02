1716b716f28fbed3c4a258c2378ca2f9c962b67e
Agustin Criado <ag-us1@hotmail.com>
2022-11-02 09:13:00 +0100

    Side bar now cleanning context ID on click and scrolling out of menu

--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -2,10 +2,17 @@ import { useContext, useEffect, useState } from "react"
 import { QueryContext } from "../context/QueryContext"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import { useMediaQuery } from "react-responsive"
 
 const Buscador = () => {
+    const initialStyles = {
+        height: "0px",
+        padding: "0 20px 0 58px"
+    }
     const [usePath, setPath] = useState("")
+    const [ useStyles, setStyles] = useState(false)
     const { setQuery } = useContext(QueryContext)
+
     useEffect(() => {
         setQuery(usePath)
     }, [usePath, setQuery])
@@ -16,10 +23,27 @@ const Buscador = () => {
         setPath(query)
     }
 
+    const isMobile = useMediaQuery({
+        query: '(max-width: 400px)'
+    })
+
+    const changeStyles = () => { setStyles(!useStyles)}
     return (
         <div className="buscador">
             <form className="input-group" onSubmit={handleSubmit}>
-                <label className="input-group-text">
+                {isMobile ? (
+                    <label onClick={changeStyles} className="input-group-text">
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    <input
+                        style={{width: useStyles ? "30px": "0px", padding: useStyles ? '0 20px 0 58px': '0px'}}
+                        type="text"
+                        name="query"
+                        className="form-control"
+                        placeholder={"Buscar Eventos por nombre o ciudad"}
+                    />
+                </label>
+                ) : (
+                    <label className="input-group-text">
                     <FontAwesomeIcon icon={faMagnifyingGlass} />
                     <input
                         type="text"
@@ -28,6 +52,8 @@ const Buscador = () => {
                         placeholder={"Buscar Eventos por nombre o ciudad"}
                     />
                 </label>
+                )}
+                
             </form>
         </div>
     )
